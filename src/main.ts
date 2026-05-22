18aa8dfd87d0725ff0748d6b54e5ac7ee40a2625
Inara <inaraoenning@gmail.com>
2026-05-22 16:55:41 -0300

    Ajuste polyfill

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-// Polyfill para sockjs-client (lib CommonJS que usa `global` do Node.js)
-(window as any).global = window;
 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
